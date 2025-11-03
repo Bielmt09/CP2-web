b6b070a9a9376e033fba1c5e940c952a1cc7cfc6
Gabriel Amparo <gabrielamparo12345@gmail.com>
2025-11-03 20:42:26 -0300

    Estruturando arquivo script

--- src/js/srcipt.js
+++ src/js/srcipt.js
@@ -1,1 +1,31 @@
-// script
+// Script geral
+
+//FUNÇÃO TROCAR
+function trocar(cor){
+  document.body.style.background=cor;
+}
+
+let imagens=[
+  "src/assets/slide1.jpg",
+  "src/assets/slide2.jpg",
+  "src/assets/slide3.jpeg",
+  "src/assets/slide4.webp",
+  "src/assets/slide5.webp",
+];
+
+//DECLARANDO AS VARIAVEIS
+
+let i=0;
+let tempo =5000; // milisegundos
+
+//função 
+
+function slideShow(){
+    document.getElementById("image").src=imagens[i];
+    i++;
+    if(i == imagens.length){
+        i=0;
+    }
+    setTimeout(slideShow,tempo)
+}
+slideShow();
